8abc586a92b83599406fab9590d1c90f6da37654
phongphamn98 <jb.phongpham98@gmail.com>
2021-09-11 15:06:26 +0700

    initialize project

--- src/index.js
+++ src/index.js
@@ -2,16 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux'
+import createSagaMiddleware from 'redux-saga'
+import { createStore, applyMiddleware, compose } from 'redux'
+import mySaga from './saga';
+import rootReducer from './reducers/rootReducer';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const sagaMiddleware = createSagaMiddleware();
+
+const composeEnhancers = typeof window === 'object' && window['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] ?
+   window['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__']({}) : compose;
+const enhancer = composeEnhancers(
+   applyMiddleware(sagaMiddleware),
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const store = createStore(
+   rootReducer, enhancer
+)
+
+sagaMiddleware.run(mySaga)
+
+ReactDOM.render(
+   <Provider store={store}>
+      <App />
+   </Provider>,
+   document.getElementById('root')
+);
